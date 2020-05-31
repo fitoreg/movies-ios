43d1a83a79e7eff58ef30269ddf33c2e293354e1
Fitore Gashi <gashi.fitore@gmail.com>
2020-05-31 14:08:47 +0200

    Create Xcode project

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Fitore Gashi on 5/31/20.
+//  Copyright © 2020 Fitore Gashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
